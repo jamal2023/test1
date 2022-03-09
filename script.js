b76d0d6a3d44ad3413a9184c6732f58a976978ad
jamal2023 <101192052+jamal2023@users.noreply.github.com>
2022-03-09 15:44:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+document.querySelector(".bars-menu").addEventListener("click", () => {
+  document.querySelector(".nav-items").classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelector(".nav-items").classList.toggle("active");
+  });
+});
+
+var project = setInterval(projectDone, 10);
+var clients = setInterval(happyClients, 10);
+var coffee = setInterval(cupsCoffee, 10);
+let count1 = 1;
+let count2 = 1;
+let count3 = 1;
+
+function projectDone() {
+  count1++;
+  document.querySelector("#number1").innerHTML = count1;
+  if (count1 == 500) {
+    clearInterval(project);
+  }
+}
+
+function happyClients() {
+  count2++;
+  document.querySelector("#number2").innerHTML = count2;
+  if (count2 == 89) {
+    clearInterval(clients);
+  }
+}
+
+function cupsCoffee() {
+  count3++;
+  document.querySelector("#number3").innerHTML = count3;
+  if (count3 == 359) {
+    clearInterval(coffee);
+  }
+}
